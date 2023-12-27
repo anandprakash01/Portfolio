544fcd30792e00bcb3af09beea44b15446ff70e3
Anand <hr.anand0101@gmail.com>
2023-12-27 19:21:05 +0530

    adding files

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+const Social = () => {
+  return (
+    <>
+      <div className="home_social">
+        <a href="" className="home_social-icon" target={"_blank"}>
+          <i class="uil uil-instagram"></i>
+        </a>
+
+        <a
+          href="https://www.linkedin.com/in/hranand/"
+          className="home_social-icon"
+          target={"_blank"}
+        >
+          <i class="uil uil-linkedin-alt"></i>
+        </a>
+
+        <a
+          href="https://github.com/anandprakash01"
+          className="home_social-icon"
+          target={"_blank"}
+        >
+          <i class="uil uil-github-alt"></i>
+        </a>
+
+        <a href="" className="home_social-icon" target={"_blank"}>
+          <i class="uil uil-whatsapp"></i>
+        </a>
+
+        <a href="" className="home_social-icon" target={"_blank"}>
+          <i class="uil uil-dribbble"></i>
+        </a>
+      </div>
+    </>
+  );
+};
+
+export default Social;
